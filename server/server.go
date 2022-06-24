3b4922277628a7f32243b71dd7c39fb10c431449
evilenzo <evilenzo@protonmail.com>
2022-06-24 05:23:51 +0300

    Removed check method

--- server/server.go
+++ server/server.go
@@ -7,12 +7,6 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func check(method string, sub string, err error) {
-	if err != nil {
-		log.Printf("Error in %v (%v): %v", method, sub, err)
-	}
-}
-
 type Server struct {
 	db dbo.DatabaseOperator
 }
