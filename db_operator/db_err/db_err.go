163c23ed7406b64fc738d98ee044fc95f7d251fc
evilenzo <evilenzo@protonmail.com>
2022-06-24 07:25:27 +0300

    Errors rework

--- db_operator/db_err/db_err.go
+++ db_operator/db_err/db_err.go
@@ -1,27 +1,13 @@
 package db_err
 
-type errorCode uint
-
-const (
-	HashWrong errorCode = iota
+import (
+	"errors"
+	"gorm.io/gorm"
 )
 
-// Implementation
+var (
+	InternalError = errors.New("internal database error")
 
-type Result[T any] interface {
-	placeholder() T
-}
-
-type Ok[T any] struct {
-	Value T
-}
-type Err[T any] struct {
-	Code errorCode
-}
-
-func (p Ok[T]) placeholder() T {
-	panic("never called")
-}
-func (p Err[T]) placeholder() T {
-	panic("never called")
-}
+	RecordNotFound = gorm.ErrRecordNotFound
+	WrongPassword  = errors.New("password hash mismatch")
+)
